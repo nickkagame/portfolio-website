49f1e082391e93b691aa297c639609630f255ac6
nick jones <nickjones@nicks-MBP.lan>
2023-03-24 15:29:20 +0000

    path correction

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ import {
   faFileArrowDown,
 } from "@fortawesome/free-solid-svg-icons";
 import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons";
-import logoN from "/Users/nickjones/Documents/Northcoders/post-graduation/portfolio-website/nick-jones/src/Assets/fonts+images/images/N.png"
+import logoN from "../Assets/fonts+images/images/N.png"
 
 export const Sidebar = () => {
   const navigate = useNavigate();
